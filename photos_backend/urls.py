c8c48b902b026976868194ac3c85fd6e593894ef
chriscohoat <chris.cohoat@gmail.com>
2012-05-22 08:23:44 -0500

    Added URL to support serving of static files.

--- photos_backend/urls.py
+++ photos_backend/urls.py
@@ -3,9 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'photos_backend.views.home', name='home'),
     url(r'^api/', include('photos_backend.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
+
