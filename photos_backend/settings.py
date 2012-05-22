f740d0c839bbb603c5e29f6c8e6591c2c1f35251
chriscohoat <chris.cohoat@gmail.com>
2012-05-22 07:07:00 -0500

    Installed admin.

--- photos_backend/settings.py
+++ photos_backend/settings.py
@@ -121,8 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     'photos_backend.photos',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
