e6199ff8eeb365102686c44dfcc2c75046b3d815
chriscohoat <chris.cohoat@gmail.com>
2012-05-22 08:31:42 -0500

    Fixed unicode representations and added photos to Admin.

--- photos_backend/photos/admin.py
+++ photos_backend/photos/admin.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id','album','relative_path',)
+    
 admin.site.register(Album, AlbumAdmin)
+admin.site.register(Photo, PhotoAdmin)
--- photos_backend/photos/models.py
+++ photos_backend/photos/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Album(models.Model):
     title = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.title
     
 class Photo(models.Model):
     album = models.ForeignKey(Album,related_name='photos')
     relative_path = models.CharField(max_length=500,null=True)
+    def __unicode__(self):
+        return '%s - %s' % (self.album,self.relative_path)
